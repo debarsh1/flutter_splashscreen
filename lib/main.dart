d7b6742ae934a6baddb581492762cf2530cdf3c7
Debarshi Chakma <noxivu@gmail.com>
2023-09-08 07:59:37 +0530

    1.0.2 Lotte Animation Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,77 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple Flutter App',
+      title: 'My Bharat App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(milliseconds: 3500),
+          () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const SecondScreen(),
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Simple Flutter App'),
-      ),
+      backgroundColor: Colors.white,
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Add your button action here
-          },
-          child: const Text('Click Me!'),
+        child: Lottie.asset(
+          'assets/splash.json', // Replace with your animation file path
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
         ),
       ),
     );
   }
 }
 
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key});
 
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("MyBharat")),
+      body: const Center(
+        child: Text(
+          "Namaste 🙏🏼",
+          textScaleFactor: 2,
+        ),
+      ),
+    );
+  }
+}
